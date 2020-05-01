5b5f980ec58d45b2266dfc610f926e0ff7fcd63c
indogusmas <indogusmasarungsamudra@gmail.com>
2020-05-01 16:33:49 +0700

    add detail

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import url
 from boards import views
 
 urlpatterns = [
-    url(r'^$',views.home, name='home'),
-    path('admin/', admin.site.urls),
+     url(r'^$', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^admin/', admin.site.urls),
 ]

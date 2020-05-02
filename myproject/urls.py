84906f0ebed56760e9a08b2468f6fd47e108f2a6
indogusmas <indogusmasarungsamudra@gmail.com>
2020-05-02 21:08:37 +0700

    bootstrap

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,7 +19,8 @@ from django.conf.urls import url
 from boards import views
 
 urlpatterns = [
-     url(r'^$', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
 ]
